2bd3a1a8ec648bdff8cd5065cd370879ed07ef62
AndyGuit <totalslash@msn.com>
2024-01-17 01:23:41 +0100

    fix: copy directory task doesn't update copy properly

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,13 +1,21 @@
 const path = require('path');
-const { mkdir, readdir, copyFile } = require('fs/promises');
+const { mkdir, readdir, copyFile, unlink } = require('fs/promises');
+const filesPath = path.join(__dirname, 'files');
+const copyPath = path.join(__dirname, 'files-copy');
 
-mkdir(path.join(__dirname, 'files-copy'), { recursive: true });
+mkdir(copyPath, { recursive: true });
 
-readdir(path.join(__dirname, 'files')).then((files) => {
+async function copyFiles() {
+  const files = await readdir(path.join(__dirname, 'files'));
+  const filesCopy = await readdir(copyPath);
+
+  // clear directory
+  for await (const copy of filesCopy) {
+    await unlink(path.join(copyPath, copy));
+  }
+
   files.forEach((file) =>
-    copyFile(
-      path.join(__dirname, 'files', file),
-      path.join(__dirname, 'files-copy', file),
-    ),
+    copyFile(path.join(filesPath, file), path.join(copyPath, file)),
   );
-});
+}
+copyFiles();
